705062f3c1ec1629ce22b4c1c81ed205cdfd257b
六月雪 <zz1231118@live.com>
2020-04-02 14:47:14 +0800

    v

--- SimpleHttpServer.Scripts/HttpFile.cs
+++ SimpleHttpServer.Scripts/HttpFile.cs
@@ -87,81 +87,85 @@ namespace SimpleHttpServer.Scripts
 
         public bool GetBoolean()
         {
-            return stream.ReadByte() == 1;
+            var value = stream.ReadByte();
+            if (value == -1) throw new EndOfStreamException();
+            return value == 1;
         }
 
         public byte GetByte()
         {
             var value = stream.ReadByte();
-            return value == -1 ? (byte)0 : (byte)value;
+            if (value == -1) throw new EndOfStreamException();
+            return (byte)value;
         }
 
         public sbyte GetSByte()
         {
             var value = stream.ReadByte();
-            return value == -1 ? (sbyte)0 : (sbyte)value;
+            if (value == -1) throw new EndOfStreamException();
+            return (sbyte)value;
         }
 
         public short GetInt16()
         {
             var buffer = new byte[sizeof(short)];
-            stream.Read(buffer, 0, buffer.Length);
+            if (stream.Read(buffer, 0, buffer.Length) != buffer.Length) throw new EndOfStreamException();
             return BitConverter.ToInt16(buffer, 0);
         }
 
         public ushort GetUInt16()
         {
             var buffer = new byte[sizeof(ushort)];
-            stream.Read(buffer, 0, buffer.Length);
+            if (stream.Read(buffer, 0, buffer.Length) != buffer.Length) throw new EndOfStreamException();
             return BitConverter.ToUInt16(buffer, 0);
         }
 
         public int GetInt32()
         {
             var buffer = new byte[sizeof(int)];
-            stream.Read(buffer, 0, buffer.Length);
+            if (stream.Read(buffer, 0, buffer.Length) != buffer.Length) throw new EndOfStreamException();
             return BitConverter.ToInt32(buffer, 0);
         }
 
         public uint GetUInt32()
         {
             var buffer = new byte[sizeof(uint)];
-            stream.Read(buffer, 0, buffer.Length);
+            if (stream.Read(buffer, 0, buffer.Length) != buffer.Length) throw new EndOfStreamException();
             return BitConverter.ToUInt32(buffer, 0);
         }
 
         public long GetInt64()
         {
             var buffer = new byte[sizeof(long)];
-            stream.Read(buffer, 0, buffer.Length);
+            if (stream.Read(buffer, 0, buffer.Length) != buffer.Length) throw new EndOfStreamException();
             return BitConverter.ToInt64(buffer, 0);
         }
 
         public ulong GetUInt64()
         {
             var buffer = new byte[sizeof(ulong)];
-            stream.Read(buffer, 0, buffer.Length);
+            if (stream.Read(buffer, 0, buffer.Length) != buffer.Length) throw new EndOfStreamException();
             return BitConverter.ToUInt64(buffer, 0);
         }
 
         public float GetSingle()
         {
             var buffer = new byte[sizeof(float)];
-            stream.Read(buffer, 0, buffer.Length);
+            if (stream.Read(buffer, 0, buffer.Length) != buffer.Length) throw new EndOfStreamException();
             return BitConverter.ToSingle(buffer, 0);
         }
 
         public double GetDouble()
         {
             var buffer = new byte[sizeof(double)];
-            stream.Read(buffer, 0, buffer.Length);
+            if (stream.Read(buffer, 0, buffer.Length) != buffer.Length) throw new EndOfStreamException();
             return BitConverter.ToDouble(buffer, 0);
         }
 
         public decimal GetDecimal()
         {
             var buffer = new byte[sizeof(decimal)];
-            stream.Read(buffer, 0, buffer.Length);
+            if (stream.Read(buffer, 0, buffer.Length) != buffer.Length) throw new EndOfStreamException();
             int lo = buffer[0] | (buffer[1] << 8) | (buffer[2] << 16) | (buffer[3] << 24);
             int mid = buffer[4] | (buffer[5] << 8) | (buffer[6] << 16) | (buffer[7] << 24);
             int hi = buffer[8] | (buffer[9] << 8) | (buffer[10] << 16) | (buffer[11] << 24);
